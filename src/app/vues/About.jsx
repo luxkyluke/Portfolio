a42fbd0165ffa3485b8e1869ff72402ba78bd6b3
luxkyluke <rock_on77500@hotmail.fr>
2017-11-10 13:18:25 +0100

    text about v1 OK

--- src/app/vues/About.jsx
+++ src/app/vues/About.jsx
@@ -14,21 +14,25 @@ export default class About extends React.Component {
         return(
             <div className="about">
                 <Logo/>
+                <div className="about__veil"></div>
                 <div className="about__wrapper">
+
                     <ProfilPic/>
                     <div className="about__texte">
                         <div className="about__texte__wrapper">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis 
-                            maxime aspernatur sit nesciunt deleniti. Molestiae alias voluptatem 
-                            quisquam incidunt, quam! Voluptates molestias reiciendis natus quibusdam, 
-                            ut neque corporis itaque facere! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis 
-                            maxime aspernatur sit nesciunt deleniti. Molestiae alias voluptatem 
-                            quisquam incidunt, quam! Voluptates molestias reiciendis natus quibusdam, 
-                            ut neque corporis itaque facere! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis 
-                            maxime aspernatur sit nesciunt deleniti. Molestiae alias voluptatem 
-                            quisquam incidunt, quam! Voluptates molestias reiciendis natus quibusdam, 
-                            ut neque corporis itaque facere! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis 
-                            maxime aspernatur sit nesciunt 
+                            <p>Hi ! I'm <span>Antoine Demière</span> a french creative developper living in Paris.</p>
+                            <p>I'm student in <a href="https://www.ingenieur-imac.fr/">IMAC</a> a school which
+                            make creative ingenieers. It taught me 3D programming, web design and filming secrets</p>
+
+                            <p>I'm passionate by the theatre and acting. I also love photography, 
+                            especially film photo, if you're interested, you can find some of my photos <a href="#">here</a>. 
+                            Then I'm also fanisated by compture art and generative design</p>
+                            
+                            <p>I'm currently looking for a 6 months internship as creative developper in
+                            web feild or for interactive installations.</p> 
+                            <p>Feel free to contact me by mail at 
+                                <a href="mailto:demiere.a@gmail.com">demiere.a@gmail.com</a>
+                            </p>
                         </div>
                     </div>
                 </div>
