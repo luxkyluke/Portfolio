fc1b0bbc193b93afae78307667af76c0e2099903
luxkyluke <rock_on77500@hotmail.fr>
2017-11-25 13:16:50 +0100

    boutons link OK

--- src/app/components/assets/Button.jsx
+++ src/app/components/assets/Button.jsx
@@ -18,11 +18,13 @@ export default class Button extends React.Component {
             return (<div style={{"display" : 'none'}}></div>);
         const myStyle = (this.props.color) ? {backgroundColor : this.props.color} : {};
         return(
-            <a onClick={this.props.click} href={this.props.link} target={this.props.target}>
-                <div className="button" style={myStyle}>
-                    {this.props.label}
-                </div>
-            </a>
+            <div className="button" >
+                <a onClick={this.props.click} href={this.props.link} target={this.props.target}>
+                    <div className="button__wrapper" style={myStyle}>
+                        {this.props.label}
+                    </div>
+                </a>
+            </div>
         );
     }
 }
